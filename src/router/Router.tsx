b0a813092c0ad7a85f542632a68d24248f54444e
Keita <fast.break.baske03@gmail.com>
2022-01-30 15:34:52 +0900

    update: ルーティング定義修正

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import { Login } from 'src/pages/login'
 import { Home } from 'src/pages/home'
-import { Setting } from 'src/pages/setting'
 import { UserManagement } from 'src/pages/user/management'
+import { Setting } from 'src/pages/setting'
 import { NotFound } from 'src/pages/notfound'
+import { HeaderLayout } from 'src/layout/HeaderLayout'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/user/management" element={<UserManagement />} />
+      <Route path="/home" element={<HeaderLayout />}>
+        <Route index element={<Home />} />
+        <Route path="user/management" element={<UserManagement />} />
+        <Route path="setting" element={<Setting />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
