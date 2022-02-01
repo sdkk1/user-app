c048a9e54c899704eafde64afb6f1c1c665142f1
Keita <fast.break.baske03@gmail.com>
2022-02-02 01:33:44 +0900

    update: LoginUserProvider を設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import theme from 'src/style/theme'
+import { LoginUserProvider } from 'src/providers/LoginUserProvider'
 import { Router } from 'src/router/Router'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <LoginUserProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </LoginUserProvider>
     </ChakraProvider>
   )
 }
